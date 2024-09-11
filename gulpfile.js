2919d50ee3921adff071e619e411abe99c465926
Fernando Saucedo <contactofernandosaucedo@gmail.com>
2024-09-11 10:32:38 -0300

    Imagenes optimizadas webp, avif y gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const autoprefixer = require('autoprefixer');
 // IMAGENES
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css( done) {
     //pasos: 1 - Identificar archivos, 2 - Compilar, 3 - Guardar el .css
@@ -19,15 +20,28 @@ function css( done) {
         
     done();
 }
+// FUNCIONES
+
 function imagenes () {
     return src( 'src/img/**/*')
         .pipe( imagemin({ optimizationLevel: 3 }) )
         .pipe( dest( 'build/img'));
 }
 function versionWebp () {
+    const opciones = {
+        quality: 50
+    }
     return src('src/img/**/*.{png,jpg}')
-        .pipe( webp() )
+        .pipe( webp(opciones) )
         .pipe( dest( 'build/img') )
+}
+function versionAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe( avif( opciones) )
+        .pipe ( dest( 'build/img') )
 }
 
 function dev() {
@@ -39,7 +53,8 @@ exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.default = series( imagenes, versionWebp, css, dev);
+exports.versionAvif = versionAvif;
+exports.default = series( imagenes, versionWebp, versionAvif, css, dev);
 
 //series - Se inicia una tarea y cuando finaliza sigue con la otra
 //parallel - Todas las tareas inician al mismo tiempo
